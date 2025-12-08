56297c573d961c5b58535774e00749c5ca57c91c
tyko.nutech@gmail.com <tyko.nutech@gmail.com>
2025-12-08 14:08:59 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MapComponent from './components/MapComponent.jsx';
+import './App.css'; // Optional: Add styles here
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='black'>Monitoring Dashboard</h1>
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
